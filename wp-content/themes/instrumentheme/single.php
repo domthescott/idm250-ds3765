b26563d265a0ca13d21e2961ce126ec06b656cfe
domthescott <114445816+domthescott@users.noreply.github.com>
2024-02-19 20:05:54 -0500

    Single.php fix

--- wp-content/themes/instrumentheme/single.php
+++ wp-content/themes/instrumentheme/single.php
@@ -1,41 +1,16 @@
-<?php
-/**
- * The Template for displaying all single posts
- *
- * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
- */
 
-get_header(); ?>
+<?php get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-			<?php
-			// Start the Loop.
-			while ( have_posts() ) :
-				the_post();
 
-				/*
-				 * Include the post format-specific template for the content. If you want
-				 * to use this in a child theme, then include a file called content-___.php
-				 * (where ___ is the post format) and that will be used instead.
-				 */
-				get_template_part( 'content', get_post_format() );
 
-				// Previous/next post navigation.
-				twentyfourteen_post_nav();
+<div class="blog-content-wrapper"> 
+<div class="blog-content">
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
-				endwhile;
-			?>
-		</div><!-- #content -->
-	</div><!-- #primary -->
+<h1>
+<?php echo get_the_title(); ?>
+</h1>
 
-<?php
-get_sidebar( 'content' );
-get_sidebar();
-get_footer();
+<?php echo get_the_content(); ?>
+</div>  
+</div>
+<?php get_footer(); ?>
