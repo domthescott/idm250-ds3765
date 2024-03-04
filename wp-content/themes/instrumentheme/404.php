2725f1ebe6596a2068313dcb3231e65c28973403
domthescott <114445816+domthescott@users.noreply.github.com>
2024-03-04 01:47:44 -0500

    404 and page fixed

--- wp-content/themes/instrumentheme/404.php
+++ wp-content/themes/instrumentheme/404.php
@@ -1,14 +1,22 @@
 <?php get_header(); ?>
 
 
+<div class="error-content">
+<img src="<?php echo get_template_directory_uri(); ?>/images/error-outline.svg" alt="Error Icon">
 
-
+<h3>404 Page Not Found</h3>
+<h4>Sorry, looks like something went wrong.
+Return <a>Home</a> and try searching again.</h4>
 
 <?php 
 wp_nav_menu([
 'theme_location' => '404-menu'
 
 ]); ?>
+
+</div>
+
+
 
   
 
---
+++ wp-content/themes/instrumentheme/page.php
@@ -0,0 +1,20 @@
+<h1>page tester</h1>
+<?php get_header(); ?>
+
+<div class="page-content-wrapper"> 
+<div class="page-content">
+
+
+<h1>
+<?php echo get_the_title(); ?>
+</h1>
+
+
+<?php echo get_the_content(); ?>
+
+</div>  
+</div>
+
+
+
+<?php get_footer(); ?>
