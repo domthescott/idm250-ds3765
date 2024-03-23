bc7c6cc51f0325f163c5da6c495b0adccc2e8db3
domthescott <114445816+domthescott@users.noreply.github.com>
2024-03-23 01:34:20 -0400

    Fixes

---
+++ wp-content/themes/instrumentheme/components/hero-readmore.php
@@ -0,0 +1,9 @@
+<div class="readmore-section">
+
+<div class="readmore-item"><h4>Item 1</h4> </div>
+
+<div class="readmore-item"> <h4>Item 2</h4> </div>
+
+<div class="readmore-item"><h4>Item 3</h4> </div>
+
+</div>
---
+++ wp-content/themes/instrumentheme/components/whatwedo-header.php
@@ -0,0 +1,13 @@
+<div class="what-we-do-section-wrapper">
+
+
+<div class="what-we-do-section">
+<h3>What We Do </h3>
+<p>
+am, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel Link dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat n Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis 
+</p>
+
+<img src="<?php echo get_template_directory_uri(); ?>/images/home-piano.png" alt="Piano Image" class="home-piano">
+
+</div>
+</div>
